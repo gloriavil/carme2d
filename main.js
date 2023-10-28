de864374a3484abd649d228a014440c5f7373a3f
gloriavil <126299100+gloriavil@users.noreply.github.com>
2023-10-27 22:35:20 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-alert('olá mundo');
+function tocasompom (idelementoaudio) {
+      document.querySelector(idelementoaudio).play();
+}
+
+const listade teclas = Document.queryselectorall('.tecla');
+
+let contador = 0;
+
+//enquanto
+while(conador < listadeteclas.length) {
+
+    const tecl = listadeteclas[contador];
+    const instrumento = tecla.classlist[1];
+    //template string
+    const idaudio = '#som_${instrumento}';
+
+    //console.log(idaudio);
+
+    tecla.onclick = function () {
+        tocasompom(idaudio);
+    }
+    
+    contador = contador + 1;
+
+    //console.log(contador);
+
+}
